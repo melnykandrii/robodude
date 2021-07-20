3f3f66d103536885d96eb40f0063a0abf0670444
Andrii Melnyk <melnik.andrey@gmail.com>
2021-07-20 16:04:21 -0400

    adding error Boundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,9 @@ import React from "react";
 import Card from "./Card";
 
 export const CardList = ({ robots }) => {
+  if (true) {
+    throw new Error("Miss card Data");
+  }
   return (
     <div>
       {robots.map((user, i) => {
