36b265a7c02d5f82f302f26beb2a06bcfbb353d3
Andrii Melnyk <melnik.andrey@gmail.com>
2021-07-20 14:54:35 -0400

    simplifying code

---
+++ src/components/SearchBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const SearchBox = ({ searchfield, searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search for robots"
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
