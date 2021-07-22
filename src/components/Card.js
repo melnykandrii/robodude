3b3b74c9435b426ee22b513cb8419553ec6c090f
Andrii Melnyk <melnik.andrey@gmail.com>
2021-07-22 13:15:52 -0400

    removing onClock func

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ({ name, email, id }) => {
+const Card = ({ name, email, id, onShowModal }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt={name} src={`https://robohash.org/name+${id}?size=200x200`} />
